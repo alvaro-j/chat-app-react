48863f02ba813c50c9c45cdb103fa53f64cc9778
alvaro-j <alvarojaime360@gmail.com>
2022-02-07 10:30:41 -0300

    import firebase and set auth,firestore variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth"
+import { useCollectionData } from "react-firebase-hooks/firestore"
+
+firebase.initializeApp({
+
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 const App = () => {
-  return <div>oi</div>;
+	return <div>oi</div>;
 };
 
 export default App;
