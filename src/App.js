328cf81ba2386f7bb1e5643421267226cbe06fbb
alvaro-j <alvarojaime360@gmail.com>
2022-02-07 11:10:30 -0300

    add api key

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-import { useAuthState } from "react-firebase-hooks/auth"
-import { useCollectionData } from "react-firebase-hooks/firestore"
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
+	apiKey: "AIzaSyDj1QoqOESBmRZ3kLg8DwIAm6CQD59OQgk",
+	authDomain: "chat-app-react-1ccb9.firebaseapp.com",
+	projectId: "chat-app-react-1ccb9",
+	storageBucket: "chat-app-react-1ccb9.appspot.com",
+	messagingSenderId: "891542763042",
+	appId: "1:891542763042:web:149f3509682e67b7d77c7b",
+	measurementId: "G-6BFFC5JYPN",
+});
 
-})
-
-const auth = firebase.auth()
-const firestore = firebase.firestore()
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 const App = () => {
 	return <div>oi</div>;
