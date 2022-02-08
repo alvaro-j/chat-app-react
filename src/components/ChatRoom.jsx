7c6cadf1f2976b157ae3a8989b2557ae42f086f1
alvaro-j <alvarojaime360@gmail.com>
2022-02-08 09:53:00 -0300

    initial code done

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const ChatRoom = () => {
-  return <div></div>;
+const ChatRoom = ({ firestore, useCollectionData }) => {
+	const messagesRef = firestore.collection("messages"); // makes reference to a firestore collection
+	const query = messagesRef.orderBy("createdAt").limit(25); // query documents in the firestore collection
+	const [messages] = useCollectionData(query, { idField: "id" }); // listen to the date in real time, it returns an array of objects
+	//👆 when the data changes, this component will render again
+	console.log(messages);
+	return (
+		<>
+			<div>{messages && messages.map((msg) => <ChatMessage key={msg.id} />)}</div>
+		</>
+	);
 };
 
 export default ChatRoom;
