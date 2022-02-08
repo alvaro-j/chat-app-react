a004bd5fc303f3aa4e464c7ea313a3bdf5dd0b3d
alvaro-j <alvarojaime360@gmail.com>
2022-02-08 09:33:04 -0300

    add comments and remove imports

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
 
 const SignIn = ({auth}) => {
   const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider)
+    const provider = new firebase.auth.GoogleAuthProvider(); //gets the google autentication
+    auth.signInWithPopup(provider) //shows google popup
   }
 	return <button onClick={signInWithGoogle}>Sign in with Google</button>;
 };
