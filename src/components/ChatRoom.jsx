8cd08cb64226193b608dd5e92f42cc76bb9d6290
alvaro-j <alvarojaime360@gmail.com>
2022-02-11 09:51:55 -0300

    change limit to limitToLast

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ChatMessage from "./ChatMessage";
 
+import "firebase/compat/firestore";
+
 const ChatRoom = ({ firestore, useCollectionData }) => {
 	const messagesRef = firestore.collection("messages"); // makes reference to a firestore collection
-	const query = messagesRef.orderBy("createdAt").limit(25); // query documents in the firestore collection
+	const query = messagesRef.orderBy("createdAt").limitToLast(25); // query documents in the firestore collection
 	const [messages] = useCollectionData(query, { idField: "id" }); // listen to the date in real time, it returns an array of objects
 	//👆 when the data changes, this component will render again
+	console.log(query);
 	return (
 		<>
-			<div>{messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}</div>{" "}
+			<div>{messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}</div>
 			{/*👆 loop for each document in the collection*/}
 		</>
 	);
