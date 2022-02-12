b9ec379863e94322aefd2baf29de3a47bd2ac81e
alvaro-j <alvarojaime360@gmail.com>
2022-02-12 09:57:51 -0300

    remove log

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -4,7 +4,6 @@ const ChatMessage = ({ message, auth }) => {
 	const { text, uid, photoURL } = message;
 	const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 	// 👆 compares the user id on the firestore document with the current user id to set the css class
-	console.log(messageClass);
 	return (
 		<div className={`message ${messageClass}`}>
 			<img src={photoURL} alt="user" />
