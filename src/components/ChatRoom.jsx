d704523ad5432b46d6559019516c75fce23a9ae8
alvaro-j <alvarojaime360@gmail.com>
2022-02-12 09:13:20 -0300

    add form

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -6,10 +6,19 @@ const ChatRoom = ({ firestore, useCollectionData, auth }) => {
 	const query = messagesRef.orderBy("createdAt").limitToLast(25); // query documents in the firestore collection
 	const [messages] = useCollectionData(query, { idField: "id" }); // listen to the date in real time, it returns an array of objects
 	//👆 when the data changes, this component will render again
+
+	const [formValue, setFormValue] = React.useState("");
+
 	return (
 		<>
-			<div>{messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} auth={auth} />)}</div>
+			<div>
+				{messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} auth={auth} />)}
+			</div>
 			{/*👆 loop for each document in the collection*/}
+			<form>
+				<input type="text" value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+				<button type="submit">Send</button>
+			</form>
 		</>
 	);
 };
