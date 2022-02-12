e0f3e240848743a834ae5bd14fc0067c0fac3a5b
alvaro-j <alvarojaime360@gmail.com>
2022-02-12 09:08:15 -0300

    display user photo on img tag

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const ChatMessage = ({ message, auth }) => {
-	const { text, uid } = message;
+	const { text, uid, photoURL } = message;
 	const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+	// 👆 compares the user id on the firestore document with the current user id to set the css class
 	console.log(messageClass);
-	return <p>{text}</p>;
+	return (
+		<div className={`message ${messageClass}`}>
+			<img src={photoURL} alt="user photo" />
+			<p>{text}</p>
+		</div>
+	);
 };
 
 export default ChatMessage;
