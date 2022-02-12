fb24380ad9c8ed900516187738457b0751993d68
alvaro-j <alvarojaime360@gmail.com>
2022-02-12 09:00:12 -0300

    add messageClass variable

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const ChatMessage = ({ message }) => {
+const ChatMessage = ({ message, auth }) => {
 	const { text, uid } = message;
+	const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+	console.log(messageClass);
 	return <p>{text}</p>;
 };
 
