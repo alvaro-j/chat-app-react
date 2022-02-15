d96d6976d4a86b1623b20f6b277415f5e1a8be2d
alvaro-j <alvarojaime360@gmail.com>
2022-02-15 10:24:55 -0300

    add new sound effect with hook

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -7,6 +7,10 @@ const SignIn = ({ auth }) => {
 		const provider = new firebase.auth.GoogleAuthProvider(); //gets the google autentication
 		auth.signInWithPopup(provider); //shows google popup
 	};
+	React.useEffect(() => {
+		const whatsIntro = new Audio("./sounds/whats2-intro.mp3");
+		whatsIntro.play();
+	}, []);
 	return (
 		<>
 			<img src="images/what2-dias.jpg" alt="contagem para whatsapp 2" className="img-count" />
